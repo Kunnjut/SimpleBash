e78d6a7a876b9fe9b37b2a6b674a308f6d96e22f
Kunnjut <153289913+Kunnjut@users.noreply.github.com>
2023-12-29 22:32:08 +0000

    -n Work

--- src/prog_1.c
+++ src/prog_1.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
+#include <string.h>
+
+int check_n (int argc, char *argv[0]);
 
 int main (int argc, char *argv[0]) {
-    int count, i = 0;
+    int count, i = 0, pN;
     char str[51];
     FILE *myfile;
-    if (argc > 2){
-        myfile = fopen(argv[argc - 1], "r");}
-    else
-        myfile = fopen(argv[1], "r");
-    i = 1;
-    while (fgets (str, 50, myfile) != NULL){
-        printf ("     %d  %s",i, str);
-        i++;
+    pN = check_n (argc, &argv[0]);
+    if (argc > 2 && pN == 1){
+        myfile = fopen(argv[argc - 1], "r");
+        i = 1;
+        while (fgets (str, 50, myfile) != NULL){
+            printf ("     %d  %s",i, str);
+            i++;
+    }
+    }
+    else {
+        myfile = fopen(argv[argc - 1], "r");
+        while (fgets (str, 50, myfile) != NULL){
+            printf ("%s", str);
+            i++;
+        }
     }
     fclose (myfile);
     return 0;
+}
+
+int check_n (int argc, char *argv[0]) {
+    int check;
+    if (argc > 2){
+        if (strcmp(argv[1], "-n")  == 0)
+        check = 1;
+    }
+    else
+        check = 0;
+    return check;
 }
