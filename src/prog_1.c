0033d7b4462c3bdff9baff36e3b21760fe67fa87
Kunnjut <153289913+Kunnjut@users.noreply.github.com>
2023-12-30 14:21:41 +0000

    Вывод разбит на функции

--- src/prog_1.c
+++ src/prog_1.c
@@ -1,39 +1,60 @@
 #include <stdio.h>
 #include <string.h>
 
-int check_n (int argc, char *argv[0]);
+int check_fl (int argc, char *argv[0]);
+int ouptput_b (int argc, char *argv[0]);
+int output_n (int argc, char *argv[0]);
+int output_without (int argc, char *argv[0]);
 
 int main (int argc, char *argv[0]) {
-    int count, i = 0, pN;
-    char str[51];
+    int i = 0, pN;
+    pN = check_fl (argc, &argv[0]);
+    if (pN == 1){
+       output_n (argc, &argv[0]);
+    }
+    else if (pN == 0) {
+      output_without (argc, &argv[0]);
+    }
+    return 0;
+}
+
+int check_fl (int argc, char *argv[0]) {
+    int check;
+    if ((argc > 2) && (strcmp(argv[1], "-n")  == 0)){
+        check = 1;
+    }
+    else if ((argc > 2) && (strcmp(argv[1], "-b")  == 0)){
+        check = 2;
+    }
+    else{
+        check = 0;
+    }
+    return check;
+}
+
+int output_n (int argc, char *argv[0]){
     FILE *myfile;
-    pN = check_n (argc, &argv[0]);
-    if (argc > 2 && pN == 1){
-        myfile = fopen(argv[argc - 1], "r");
+    char str[51];
+    int i = 0;
+    myfile = fopen(argv[argc - 1], "r");
         i = 1;
         while (fgets (str, 50, myfile) != NULL){
             printf ("     %d  %s",i, str);
             i++;
     }
-    }
-    else {
-        myfile = fopen(argv[argc - 1], "r");
-        while (fgets (str, 50, myfile) != NULL){
-            printf ("%s", str);
-            i++;
-        }
-    }
     fclose (myfile);
     return 0;
 }
 
-int check_n (int argc, char *argv[0]) {
-    int check;
-    if (argc > 2){
-        if (strcmp(argv[1], "-n")  == 0)
-        check = 1;
-    }
-    else
-        check = 0;
-    return check;
+int output_without (int argc, char *argv[0]){
+    FILE *myfile;
+    char str[51];
+    int i = 0;
+    myfile = fopen(argv[argc - 1], "r");
+        while (fgets (str, 50, myfile) != NULL){
+            printf ("%s", str);
+            i++;
+        }
+        fclose (myfile);
+        return 0;
 }
