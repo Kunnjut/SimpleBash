349771556bde5ea64c1e720d4a421f3459053e43
Kunnjut <153289913+Kunnjut@users.noreply.github.com>
2024-01-06 19:18:41 +0000

    Сделана проверка на открытие файла == NULL

--- src/prog_1.c
+++ src/prog_1.c
@@ -1,6 +1,15 @@
 #include "prog_1.h"
+#include <stdio.h>
 
 int main (int argc, char *argv[0]) {
-    check_fl (argc, &argv[0]);
+    FILE *check_null;
+    check_null = fopen(argv[argc - 1], "r");
+    if ( check_null == NULL){
+        printf ("No such file or directory\n");
+    }
+    else {
+        fclose(check_null);
+        check_fl (argc, &argv[0]);
+    }
     return 0;
 }
