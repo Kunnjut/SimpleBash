8ac20844d7ef5aca5d3b7b002c3e203002bb2132
Kunnjut <153289913+Kunnjut@users.noreply.github.com>
2024-01-09 09:24:15 +0000

    Начало Грепа

---
+++ src/grep/grep.c
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include <string.h>
+#include <grep.h>
+#include <getopt.h>
+#include <regex.h>
+#include <stdio.h>
--- src/grep/prog_4.c
+++
@@ -1,7 +0,0 @@
-#include <regex.h>
-
-int main (){
-    char pattern[] = "ab";
-    char text[] = "abcabcd a b ababcdef bac";
-    return 0;      
-}  
