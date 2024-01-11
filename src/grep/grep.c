0179ce9a2c75b185de353347fb1c3972d6919e3b
Kunnjut <153289913+Kunnjut@users.noreply.github.com>
2024-01-11 15:50:21 +0000

    grep 17:25

--- src/grep/grep.c
+++ src/grep/grep.c
@@ -4,3 +4,76 @@
 #include <getopt.h>
 #include <regex.h>
 #include <stdio.h>
+#include "grep.h"
+
+
+arguments arguments_parser (int argc, char* argv[]){
+    arguments arg = {0};
+    int opt;
+    while ((opt = getopt(argc, argv, "e:ivclnhsf:o")) != -1) {
+        switch (opt) {
+            case 'e':
+                arg.e = 1;
+                arg.pattern = optarg;
+                break;
+            case 'i':
+                //???
+                break;
+            case 'v':
+                arg.v = 1;
+                break;
+            case 'c':
+                arg.c = 1;
+                break;
+            case 'l':
+                arg.c = 1;
+                arg.l = 1;
+                break;
+            case 'n':
+                arg.n = 1;
+                break;
+            case 'h':
+                arg.h = 1;
+                break;
+            case 's':
+                arg.s = 1;
+                break;
+            case 'f':
+                arg.f = 1;
+                //???
+                break;
+            case 'o':
+                arg.o = 1;
+                break;
+        }
+    }
+    return arg;
+}
+
+void output_line (char* line, int n) {
+    for (int i = 0; i < n ; i++){
+        putchar (line[i]);
+    }
+    //?
+}
+
+void processFile (arguments arg, char* path, regex_t* reg) {
+    FILE* f = fopen (path, "r");
+    if (f == NULL) {
+        perror (path);
+        exit(1);
+    }
+    char* line = NULL;
+    size_t memlen = 0;
+    int read = 0;
+    read = getline (&line, &memlen, f);
+    //??
+    while (read != -1) {
+        //??
+        read = getline (&line, &memlen, f);
+        //??
+    }
+    free (line);
+    //?
+    fclose(f);
+}
