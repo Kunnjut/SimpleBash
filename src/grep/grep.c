f1809c93271a78a7d0a44a80bdc5f8282a6ca40b
Kunnjut <153289913+Kunnjut@users.noreply.github.com>
2024-01-29 18:00:35 +0000

    Компоновка кода

--- src/grep/grep.c
+++ src/grep/grep.c
@@ -5,31 +5,20 @@
 #include <stdio.h>
 #include "grep.h"
 
+arguments arguments_parser (int argc, char* argv[]);
+void compreg (arguments arg, int argc, char** argv);
+void pattern_add (arguments* arg, char* pattern);
+void reg_file (arguments* arg, char* filepath);
+void compreg_line (char* line, int n);
+void print_match (regex_t *re, char *line);
+void Obrabotka_File (arguments arg, char* path, regex_t* reg);
 
-void pattern_add (arguments* arg, char* pattern){
-    if (arg->len_pattern != 0) {
-        strcat(arg->pattern + arg->len_pattern, "|");
-        arg->len_pattern++;
-    }
-    arg->len_pattern += sprintf(arg->pattern + arg->len_pattern, "(%s)", pattern);
-}
 
-void add_reg_from_file (arguments* arg, char* filepath) {
-    FILE* f = fopen(filepath, "r");
-    if (f == NULL){
-        if (!arg->s) perror (filepath);
-        exit(1);
-    }
-    char* line = NULL;
-    size_t memlen = 0;
-    int read = getline(&line, &memlen, f);
-    while (read != -1) {
-        if (line[read - 1] == '\n') line[read - 1] = '\0';  // Убираем символ перевода строки что бы он считался как паттерн
-        pattern_add(arg, line);
-        read = getline(&line, &memlen, f);
-    }
-    free (line);
-    fclose(f);
+
+int main (int argc, char** argv) {
+    arguments arg = arguments_parser(argc, argv);
+    compreg (arg, argc, argv);
+    return 0;
 }
 
 arguments arguments_parser (int argc, char* argv[]){
@@ -65,7 +54,7 @@ arguments arguments_parser (int argc, char* argv[]){
                 break;
             case 'f':
                 arg.f = 1;
-                add_reg_from_file(&arg, optarg);
+                reg_file(&arg, optarg);
                 break;
             case 'o':
                 arg.o = 1;
@@ -82,7 +71,48 @@ arguments arguments_parser (int argc, char* argv[]){
     return arg;
 }
 
-void output_line (char* line, int n) {
+
+void compreg (arguments arg, int argc, char** argv) {
+    regex_t re;
+    int error = regcomp(&re, arg.pattern, REG_EXTENDED | arg.i);
+    if (error){
+        perror("Error");
+    }
+    for (int i = optind; i < argc; i++){
+        Obrabotka_File (arg, argv[i], &re);
+    }
+}
+
+
+void pattern_add (arguments* arg, char* pattern){  // Соединяет паттерны
+    if (arg->len_pattern != 0) {
+        strcat(arg->pattern + arg->len_pattern, "|");
+        arg->len_pattern++;
+    }
+    arg->len_pattern += sprintf(arg->pattern + arg->len_pattern, "(%s)", pattern);
+}
+
+void reg_file (arguments* arg, char* filepath) {   // Чтение паттернов из файла
+    FILE* f = fopen(filepath, "r");
+    if (f == NULL){
+        if (!arg->s) perror (filepath);  // Не выводит сообщение об ошибке при открытии файла при флаге s
+        exit(1);
+    }
+    char* line = NULL;
+    size_t lstr = 0;
+    int read = getline(&line, &lstr, f);
+    while (read != -1) {
+        if (line[read - 1] == '\n') line[read - 1] = '\0';  // Убираем символ перевода строки что бы он считался как паттерн
+        pattern_add(arg, line);
+        read = getline(&line, &lstr, f);
+    }
+    free (line);
+    fclose(f);
+}
+
+
+
+void compreg_line (char* line, int n) {
     for (int i = 0; i < n ; i++){
         putchar (line[i]);
     }
@@ -91,52 +121,48 @@ void output_line (char* line, int n) {
 
 void print_match (regex_t *re, char *line){
     regmatch_t match;
-    int offset = 0;
+    int sdvig = 0;
     while (1){
-        int result = regexec(re, line + offset, 1, &match, 0);
+        int result = regexec(re, line + sdvig, 1, &match, 0);
         if (result != 0)
         {
             break;
         }
-        
         for(int i = match.rm_so; i < match.rm_eo; i++){      // so - начало совпадения, eo - конец
             putchar(line[i]);
         }
         putchar ('\n');
-        offset += match.rm_eo;
+        sdvig += match.rm_eo;
     }
-
-
 }
 
-void processFile (arguments arg, char* path, regex_t* reg) {
+void Obrabotka_File (arguments arg, char* path, regex_t* reg) {
     FILE* f = fopen (path, "r");
     if (f == NULL) {
         if (!arg.s) perror (path);
         exit(1);
     }
     char* line = NULL;
-    size_t memlen = 0;
+    size_t lstr = 0;
     int read = 0;
-    int line_count = 1;
+    int number_line = 1;
     int c = 0, for_l = 0;
-    read = getline (&line, &memlen, f);
-    //??
+    read = getline (&line, &lstr, f);
     while (read != -1) {
         int result = regexec (reg, line, 0, NULL, 0);
         if ((result == 0 && !arg.v) || (arg.v && result != 0)) {
             for_l = 1;
             if (!arg.c && !arg.l){
-                if (arg.n) printf ("%d:", line_count);                // Выводит номер строки в которой нашел совпадения (-n)
+                if (arg.n) printf ("%d:", number_line);                // Выводит номер строки в которой нашел совпадения (-n)
                 if (arg.o) {
                     print_match(reg, line);
                 } 
-                else output_line(line, read);
+                else compreg_line(line, read);
             }
             c++;
         }
-        read = getline (&line, &memlen, f);
-        line_count++;
+        read = getline (&line, &lstr, f);
+        number_line++;
     }
     free (line);
     if (arg.c && !arg.l) {
@@ -147,21 +173,4 @@ void processFile (arguments arg, char* path, regex_t* reg) {
         printf ("%s\n", path);
     }
     fclose(f);
-}
-
-//void processLine () {
-
-void output (arguments arg, int argc, char** argv) {
-    regex_t re;
-    int error = regcomp(&re, arg.pattern, REG_EXTENDED | arg.i);
-    if (error) perror("Error");
-    for (int i = optind; i < argc; i++){
-        processFile (arg, argv[i], &re);
-    }
-}
-// -i
-int main (int argc, char** argv) {
-    arguments arg = arguments_parser(argc, argv);
-    output (arg, argc, argv);
-    return 0;
 }
